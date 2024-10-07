6215af4368f593a49662e8055a2f437b5c708ed0
mariana f. <91221958+marianaolfr@users.noreply.github.com>
2024-10-07 16:33:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
-from flask import Flask, request, make_response, redirect, abort;
-app = Flask(__name__);
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+def gerar_cabecalho():
+    return "<h1>Avaliação contínua: Aula 030</h1>"
 
 @app.route('/')
 def index():
-    return '<h1>Avaliação contínua: Aula 030</h1><ul><li><a href="/">Home</a></li><li><a href="/user/Thomas Luizon/PT3019438/IFSP">Identificação</a></li><li><a href="/contextorequisicao">Contexto da requisição</a></li></ul>'
+    return f"""
+    {gerar_cabecalho()}
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/user/Thomas Luizon/PT3019438/IFSP">Identificação</a></li>
+        <li><a href="/contextorequisicao">Contexto da requisição</a></li>
+    </ul>
+    """
 
 @app.route('/user/<name>/<prontuario>/<instituicao>')
 def user(name, prontuario, instituicao):
-    return f'<h1>Avaliação contínua: Aula 030</h1><h2>Aluno: {name}</h2><h2>Prontuário: {prontuario}</h2><h2>Instituição: {instituicao}</h2><p><a href="/">Voltar</a></p>'
+    return f"""
+    {gerar_cabecalho()}
+    <h2>Aluno: {name}</h2>
+    <h2>Prontuário: {prontuario}</h2>
+    <h2>Instituição: {instituicao}</h2>
+    <p><a href="/">Voltar</a></p>
+    """
 
 @app.route('/contextorequisicao')
 def contextorequisicao():
     user_agent = request.headers.get('User-Agent')
     remote_ip = request.remote_addr
     host = request.host
-    return f'<h1>Avaliação contínua: Aula 030</h1><h2>Seu navegador é: {user_agent}</h2><h2>O IP do computador remoto é: {remote_ip}</h2><h2>O host da aplicação é: {host}</h2><p><a href="/">Voltar</a></p>'
+    return f"""
+    {gerar_cabecalho()}
+    <h2>Seu navegador é: {user_agent}</h2>
+    <h2>O IP do computador remoto é: {remote_ip}</h2>
+    <h2>O host da aplicação é: {host}</h2>
+    <p><a href="/">Voltar</a></p>
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True)
